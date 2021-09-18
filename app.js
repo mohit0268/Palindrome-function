7a982bd5ec52a243ae4e69f6655b7d16f9cb946f
mohit0268 <mohit0268@users.noreply.github.com>
2021-09-18 15:17:23 +0530

    this function converts date format into string format

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function reverseString(str){
     var reverseStr=reverseCharacter.join('');
     return reverseStr;
 }
-
+//checking string is palindrome or not
 function isPalindrome(str){
     var reverse=reverseString(str);
     if(str === reverse){
@@ -17,5 +17,60 @@ function isPalindrome(str){
   return reverse;
 
 }
+// function convertDateToString(day,month,year){
+//     var date={
+//         day:15,
+//         month:1,
+//         year:2020
+//     }
+//     if(date.day<10){
+//         date.day='0'+date.day;
+//     }
+//     else{
+//         date.day=date.day.toString();
+//     }
+//     if(date.month<10){
+//         date.month='0'+date.month;
+//     }
+//     else{
+//         date.month=date.month.toString();
+//     }
+//     date.year=date.year.toString();
+//     return date;
+    
 
+// }
+// console.log(convertDateToString(2,10))
+
+
+
+//converting date format into string format
+function convertDateToString(date){
+    var dateStr={
+        day:'',
+        month:'',
+        year:''
+    }
+    if(date.day<10){
+        dateStr.day='0' + date.day;
+    }
+    else{
+        dateStr.day=date.day.toString();
+    }
+    if(date.month<10){
+        dateStr.month='0' + date.month;
+    }
+    else{
+        dateStr.month=date.month.toString();
+    }
+    dateStr.year=date.year.toString();
+    return dateStr;
+}
+var date={
+    day:1,
+    month:2,
+    year:2020
+};
+
+console.log(convertDateToString(date))
 
